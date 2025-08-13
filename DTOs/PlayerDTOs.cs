478ff9506f54a564d9577b4bd890ec674d9b4a9d
Demond Balentine <demond.balentine@atlasschool.com>
2025-08-13 10:06:40 -0500

    Create PlayerDTOs.cs

---
+++ DTOs/PlayerDTOs.cs
@@ -0,0 +1,8 @@
+public class PlayerDTO
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Position { get; set; }
+    public string Team { get; set; }
+}
