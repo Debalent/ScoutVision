28a208ad8e2e768af94f1be225f7f8eb7e293ac8
Demond Balentine <demond.balentine@atlasschool.com>
2025-08-13 10:07:45 -0500

    Create PlayerContactInfoDTO.cs

---
+++ DTOs/PlayerContactInfoDTO.cs
@@ -0,0 +1,14 @@
+public class PlayerContactInfoDTO
+{
+    public string PhoneNumber { get; set; }
+    public string EmailAddress { get; set; }
+    public string StreetAddress { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+
+    public string InstagramHandle { get; set; }
+    public string TwitterHandle { get; set; }
+    public string TikTokHandle { get; set; }
+    public string LinkedInProfile { get; set; }
+}
